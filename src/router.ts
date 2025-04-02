db622534d62d83d1311187d0f8a5e020f9cd6cc6
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2025-04-02 14:40:27 +0530

    Added validation using expreess-validator

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -6,26 +8,50 @@ interface CustomRequest extends Request {
     shh_secret?: string;
 }
 
-// router.get('/product', ()=>{})
 router.get('/product', (req: CustomRequest, res: Response) => {
     res.json({ message: req.shh_secret });
 });
 router.get('/product/:id', () => { })
-router.put('/product/:id', () => { })
-router.post('/product', () => { })
+router.put('/product/:id',body('name').isString(), handleInputErrors ,(req, res) => { 
+    
+})
+router.post('/product',[body('name').isString(), body('belongsToId').isString()], handleInputErrors ,(req, res) => {})
 router.delete('/product/:id', () => { })
 
 
 router.get('/update', () => { })
 router.get('/update/:id', () => { })
-router.put('/update/:id', () => { })
-router.post('/update', () => { })
+
+
+router.put('/update/:id', 
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGESS', 'LIVE', 'DEPRECATED', 'ARCHIVED']).optional(),
+    body('version').optional(), 
+    () => { })
+
+
+router.post('/update', 
+    body('title').exists(),
+    body('body').exists(),
+    body('status').isIn(['IN_PROGESS', 'LIVE', 'DEPRECATED', 'ARCHIVED']).exists(),
+    body('version').exists(), 
+    () => { })
+
+     
 router.delete('/update/:id', () => { })
 
 router.get('/updatepoint', () => { })
 router.get('/updatepoint/:id', () => { })
-router.put('/updatepoint/:id', () => { })
-router.post('/updatepoint', () => { })
+router.put('/updatepoint/:id',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+ () => { })
+router.post('/updatepoint',
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => { })
 router.delete('/updatepoint/:id', () => { })
 
 export default router;
